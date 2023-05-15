0cfff72185f358b021defd4dd0e1478661e87138
Dnysss <denysmchal@gmail.com>
2023-05-15 19:06:39 -0300

    added cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const router = require('./router')
+const cors = require('cors');
+const router = require('./router');
 
 const app = express();
 
 //app.get('/', (request, response) => response.status(200).send('Hello, Friend!'));
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 module.exports = app;
